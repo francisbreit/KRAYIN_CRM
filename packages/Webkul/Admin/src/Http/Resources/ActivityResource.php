4e1ca8ee773a7a7f376079f1350738eccc38c1c9
Francis Breit <francisbreit@gmail.com>
2025-01-25 18:47:04 -0300

    Update ActivityResource.php
    correção de timezone, data e hora

--- packages/Webkul/Admin/src/Http/Resources/ActivityResource.php
+++ packages/Webkul/Admin/src/Http/Resources/ActivityResource.php
@@ -21,15 +21,28 @@ class ActivityResource extends JsonResource
             'type'          => $this->type,
             'comment'       => $this->comment,
             'additional'    => is_array($this->resource->additional) ? $this->resource->additional : json_decode($this->resource->additional, true),
-            'schedule_from' => $this->schedule_from,
-            'schedule_to'   => $this->schedule_to,
+            'schedule_from' => $this->formatDate($this->schedule_from),
+            'schedule_to'   => $this->formatDate($this->schedule_to),
             'is_done'       => $this->is_done,
             'user'          => new UserResource($this->user),
             'files'         => ActivityFileResource::collection($this->files),
             'participants'  => ActivityParticipantResource::collection($this->participants),
             'location'      => $this->location,
-            'created_at'    => $this->created_at,
-            'updated_at'    => $this->updated_at,
+            'created_at'    => $this->formatDate($this->created_at),
+            'updated_at'    => $this->formatDate($this->updated_at),
         ];
+    }
+
+    /**
+     * Helper function to format dates.
+     *
+     * @param  string|null $date
+     * @return string|null
+     */
+    protected function formatDate($date)
+    {
+        return $date 
+            ? \Carbon\Carbon::parse($date)->timezone('America/Sao_Paulo')->format('d/m/Y H:i:s') 
+            : null;
     }
 }
